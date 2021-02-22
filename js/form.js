6adac5a0a3547fb539ea098e2fe8a9acf40a2489
kotser1 <newelvin@gmail.com>
2021-02-22 22:25:36 +0300

    Выбор опции меняет атрибуты min и placeholder у поля «Цена за ночь»

--- js/form.js
+++ js/form.js
@@ -1,7 +1,20 @@
 const offerForm = document.querySelector('.ad-form');
-
 const checkinTime = offerForm.querySelector('#timein');
 const checkoutTime = offerForm.querySelector('#timeout');
+const offerType = offerForm.querySelector('#type');
+const offerPrice = offerForm.querySelector('#price');
+
+const DEFAULT_MIN_PRICE = {
+  palace: 10000,
+  flat: 1000,
+  house: 5000,
+  bungalow: 0,
+};
+
+offerType.addEventListener('change', function () {
+  offerPrice.placeholder = DEFAULT_MIN_PRICE[offerType.value];
+  offerPrice.min = DEFAULT_MIN_PRICE[offerType.value];
+});
 
 checkinTime.addEventListener('change', function () {
   checkoutTime.value = checkinTime.value;
