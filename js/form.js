65f914c37e6e28f5cb168572ee30a6a49ba2b34c
kotser1 <newelvin@gmail.com>
2021-03-05 01:05:59 +0300

    начало работы над валидацией формы

--- js/form.js
+++ js/form.js
@@ -11,6 +11,43 @@ const DEFAULT_MIN_PRICE = {
   bungalow: 0,
 };
 
+const numberOfGuests = {
+  1: ['1'],
+  2: ['1', '2'],
+  3: ['1', '2', '3'],
+  100: ['0'],
+};
+
+const selectCapacity = offerForm.querySelector('#capacity');
+const capacityOptions = selectCapacity.querySelectorAll('option');
+const selectRooms = offerForm.querySelector('#room_number');
+
+const validateRooms = () => {
+  const roomValue = selectRooms.value;
+
+  capacityOptions.forEach((option) => {
+    // Что проверяет это условие:
+    // Мы проверяем наличие option.value (кол-во гостей) в значении свойства объекта numberOfGuests. Значения у этого объекта являются массивами.
+    // Используя метод indexOf мы получаем индекс элемента массива (проверяем наличие элемента в массиве).
+    // Если элемент есть в массиве, то индекс будет >=0 . А если indexOf возвращает -1 , то это значит что элемента в массиве нет и ему ставим атрибут disabled.
+    let isDisabled = (numberOfGuests[roomValue].indexOf(option.value) === -1);
+
+    option.selected = numberOfGuests[roomValue][0] === option.value;
+    option.disabled = isDisabled;
+    option.hidden = isDisabled;
+  });
+};
+
+const onRoomNumberChange = () => {
+  validateRooms();
+};
+
+validateRooms();
+selectRooms.addEventListener('change', onRoomNumberChange);
+
+
+// addValidation();
+
 offerType.addEventListener('change', function () {
   offerPrice.placeholder = DEFAULT_MIN_PRICE[offerType.value];
   offerPrice.min = DEFAULT_MIN_PRICE[offerType.value];
