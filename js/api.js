aa71e4cc99e5c87667e97ece2946dc0406fe1dc5
kotser1 <59193071+kotser1@users.noreply.github.com>
2021-03-11 03:27:06 +0300

    добавил обработку ошибки при загрузке данных с сервера

--- js/api.js
+++ js/api.js
@@ -1,7 +1,11 @@
 import {renderElements} from './map.js';
+import {showAlert} from './util.js';
 
-fetch('https://22.javascript.pages.academy/keksobooking/data')
+fetch('https://22.javascript.pages.academy/keksobooking/data222')
   .then((response) => response.json())
   .then((homes) => {
     renderElements(homes);
+  })
+  .catch(() => {
+    showAlert('Не удалось загрузить список объектов. Попробуйте ещё раз');
   });
--- js/util.js
+++ js/util.js
@@ -1,3 +1,5 @@
+const ALERT_SHOW_TIME = 5000;
+
 const getRandomNumber = (min, max, digit = 0) => {
   if (min < 0 || max < 0) {
     return -1;
@@ -36,4 +38,25 @@ const numDecline = (num, nominative, genitiveSingular, genitivePlural) => {
   }
 };
 
-export {getRandomNumber, getRandomArrayElement, shuffleArray, numDecline};
+const showAlert = (message) => {
+  const alertContainer = document.createElement('div');
+  alertContainer.style.zIndex = 100;
+  alertContainer.style.position = 'absolute';
+  alertContainer.style.left = 0;
+  alertContainer.style.top = 0;
+  alertContainer.style.right = 0;
+  alertContainer.style.padding = '10px 3px';
+  alertContainer.style.fontSize = '20px';
+  alertContainer.style.textAlign = 'center';
+  alertContainer.style.backgroundColor = 'red';
+
+  alertContainer.textContent = message;
+
+  document.body.append(alertContainer);
+
+  setTimeout(() => {
+    alertContainer.remove();
+  }, ALERT_SHOW_TIME);
+}
+
+export {getRandomNumber, getRandomArrayElement, shuffleArray, numDecline, showAlert};
